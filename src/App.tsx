dd9c905213d93f852ffd970d46a3ee31441833c6
Davian <rehan.davian@gmail.com>
2024-12-18 22:47:47 +0000

    UI displays back-end data

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css'
 import axios from "axios"
 
 function App() {
-  const [backendData, setBackendData] = useState([{}]);
+  const [backendData, setBackendData] = useState([]);
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:5000/api");
+    setBackendData(response.data.users);
     console.log(response.data.users);
   };
 
@@ -19,6 +20,14 @@ function App() {
   return (
     <div>
       <h1>Welcome!</h1>
+      {
+        backendData.map((user, index) => (
+          <div key={index}>
+            <p>{user}</p>
+            <br/>
+          </div>
+        ))
+      }
     </div>
   )
 }
