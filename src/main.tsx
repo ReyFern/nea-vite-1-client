81ed4bc7bf0133de9593e9de611cce0cc6298c20
Davian <rehan.davian@gmail.com>
2024-12-21 10:32:13 +0000

    Deleted Home React component, added router to main.tsx, and added test code to CreateCharacter page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import CreateCharacter from './pages/CreateCharacter.tsx'
+import Party from './pages/Party.tsx'
+import Profile from './pages/Profile.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+  },
+  {
+    path: "/CreateCharacter",
+    element: <CreateCharacter />,
+  },
+  {
+    path: "/Party",
+    element: <Party />,
+  },
+  {
+    path: "/Profile",
+    element: <Profile />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/CreateCharacter.tsx
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+
+export default function CreateCharacter() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Character Creator</h1>
+        </div>
+    );
+}
