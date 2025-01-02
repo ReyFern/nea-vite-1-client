399e8aa28146ee4156544b9b9a2d9e8ecb74f293
Davian <rehan.davian@gmail.com>
2025-01-02 00:24:00 +0000

    Made return of Profile page conditional on token stored in localStorage, Sign In page now receives json token from back-end

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,24 +5,39 @@ import axios from 'axios';
 
 export default function Profile() {
     const [backendData, setBackendData] = useState([]);
+    let signedIn:string = "unauthorised";
 
     const fetchAPI = async () => {
-        const response = await axios.get("http://localhost:5000/auth");
+        const response = await axios.get("http://localhost:5000/signin-request");
         setBackendData(response.data.auth);
         console.log(response.data.auth);
+        if (response.data.auth == "success"){
+            signedIn = "authorised";
+            localStorage.setItem("token", signedIn);
+        }
     };
 
-    useEffect(() => {
-        fetchAPI();
-    }, []);
+    localStorage.setItem("token", "unauthorised");
 
-    return (
-        <div>
-            <Navbar/>
-            <h2>It seems you aren't logged in.</h2>
-            <h2>You can register if you are a new user or log in if you are a returning user.</h2>
-            <a href="/Register"><button><h2>Register</h2></button></a>
-            <a href="/SignIn"><button><h2>Sign In</h2></button></a>
-        </div>
-    );
+    if (localStorage.getItem("token") == "authorised") {
+        return (
+            <div>
+                <Navbar/>
+                <div>{backendData}</div>
+                <h2>Logged in</h2>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                <Navbar/>
+                <div>{backendData}</div>
+                <h2>It seems you aren't logged in.</h2>
+                <h2>You can register if you are a new user or log in if you are a returning user.</h2>
+                <a href="/Register"><button><h2>Register</h2></button></a>
+                <a href="/SignIn"><button><h2>Sign In</h2></button></a>
+            </div>
+        );
+    }
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,40 @@
 import Navbar from '../components/Navbar';
+import '../App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function SignIn() {
-    return (
-        <div>
-            <Navbar/>
-            <form id="user_data" method="post" action="http://localhost:5000/signin-request">
-                <label htmlFor="username">Username: </label>
-                <input type="text" id="username" name="username"/><br/><br/>
-                <label htmlFor="password">Password: </label>
-                <input type="text" id="password" name="password"/><br/><br/>
-                <input type="submit" value="Submit" className='submit-input'/>
-            </form>
-        </div>
-    );
+    const [backendData, setBackendData] = useState([]);
+
+    const fetchAPI = async () => {
+        const response = await axios.get("http://localhost:5000/signin-request");
+        setBackendData(response.data.auth);
+        console.log(response.data.auth);
+    }
+
+    const apiCall = () => {
+        axios.get('http://localhost:5000/signin-request').then((data) => {
+          //this console.log will be in our frontend console
+          localStorage.setItem("token", data.data);
+          console.log(localStorage.getItem("token"));
+        });
+    }
+
+    localStorage.setItem("token", "unauthorised");
+
+    if (localStorage.getItem("token") == "unauthorised") {
+        return (
+            <div>
+                <Navbar/>
+                <iframe name="target-frame" id="target-frame"></iframe>
+                <form id="user_data" method="get" action="http://localhost:5000/signin-request" target="target-frame">
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" id="username" name="username"/><br/><br/>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" name="password"/><br/><br/>
+                    <a href="google.com"><input type="submit" value="Submit" className='submit-input' onClick={fetchAPI}/></a>
+                </form>
+            </div>
+        )
+    };
 }
